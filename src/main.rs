c728e5406e243f8bc0a47fc068a4802afec4c9c2
Ian Donaldson <dragomundo@outlook.com>
2026-03-13 19:29:29 -0500

    No autocomplete? NO thank you!

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::error::Error;
+use slint::slint;
 
 slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
+#[cfg_attr(target_arch = "wasm32",
+    wasm_bindgen::prelude::wasm_bindgen(start))]
+pub fn main() {
+    use slint::Model;
 
-    ui.run()?;
+    let main_window = MainWindow::new().unwrap();
 
-    Ok(())
+    // Fetch the tiles from the model
+    let mut tiles: Vec<TileData> = main_window.get_letter_tiles().iter().collect();
+    // Duplicate them to ensure that we have pairs
+    // for i in 0..4 {
+    //     tiles.extend(tiles.clone());
+    // };
+
+    // Randomly mix the tiles
+    use rand::seq::SliceRandom;
+    let mut rng = rand::rng();
+    tiles.shuffle(&mut rng);
+
+    // Assign the shuffled Vec to the model property
+    let tiles_model = std::rc::Rc::new(slint::VecModel::from(tiles));
+    main_window.set_letter_tiles(tiles_model.clone().into());
+
+    main_window.run().unwrap();
 }
